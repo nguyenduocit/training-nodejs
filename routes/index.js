c750686fe8ee1be3858f47575cadfde3fffdb8df
nguyenduocit <duocnguyenit1994@gmail.com>
2018-05-03 10:40:26 +0700

    route anh bien mang trong node js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/tin-tuc', function(req, res, next) {
+	res.render('tin-tuc', {title:'Tin tuc', noidung : 'noidung tin tuc'});
+});
+
+router.get('/fedu.vn', function(req, res, next) {
+	var data = { data : ['html', 'css', 'java', 'php']};
+
+	res.render('fedu', {data : data});
+
+});
+
 module.exports = router;
